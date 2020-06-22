9298dc417278e4666b7fc6c80eec8af3c5ee3329
tej sai ram j <tejsairam@gmail.com>
2020-06-22 07:00:30 +0000

    added more examples

--- main.py
+++ main.py
@@ -5,16 +5,33 @@ turtle = turtle.Turtle()
 
 # defining a function for square
 
-def square():
-  turtle.forward(90)
+def square(length):
+  turtle.forward(length)
   turtle.left(90)
-  turtle.forward(90)
+  turtle.forward(length)
   turtle.left(90)
-  turtle.forward(90)
+  turtle.forward(length)
   turtle.left(90)
-  turtle.forward(90)
+  turtle.forward(length)
   turtle.left(90)
 
-square()
-turtle.forward(90)
-square()
+square(120*2)
+square(120 / 2)
+turtle.forward(60)
+square(120/2)
+
+print(125.4 // 4)
+
+print(True)
+print(False)
+
+is_john_killer = True
+is_bob_killer = False
+
+if is_bob_killer == True:
+  print("Bob is the Killer")
+else:
+  print("John is the killer")
+
+
+print(5 == 5)
