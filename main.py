7a4e51138d11c14ef6c7b89aafe113fde46b937e
tej sai ram j <tejsairam@gmail.com>
2020-06-22 05:39:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+
+# we created an object called turtle
+turtle = turtle.Turtle()
+
+# defining a function for square
+
+def square():
+  turtle.forward(90)
+  turtle.left(90)
+  turtle.forward(90)
+  turtle.left(90)
+  turtle.forward(90)
+  turtle.left(90)
+  turtle.forward(90)
+  turtle.left(90)
+
+square()
+turtle.forward(90)
+square()
